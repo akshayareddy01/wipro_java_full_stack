207477006bedc15d48cf88bf51f9c108fbf2b36e
akshayareddy01 <akshayapasham2805@gmail.com>
2025-07-16 15:46:01 +0530

    Create ConcatenateStrings.java

---
+++ ConcatenateStrings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ConcatenateStrings {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        String s=s1.concat(s2);
+      System.out.println(s.toLowerCase());
+    }
+  }
